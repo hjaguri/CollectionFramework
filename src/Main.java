9918ea164538bd853b90899d6e54d79fca661997
Hansraj Jaguri <hjaguri27@gmail.com>
2023-10-06 18:43:52 +0530

    Collection Framework introduction

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+//Difference Between Array and Collection Framework
+//1.Arrays can store  both the data type like primitive and non-primitive
+// - object we can also store.
+//Array only store homogeneous type of data (mean same type of data).
+//Array sized is fixed (we can not change the size at run time).
+//Array are inbuilt feature in java and we have to develop the algorithm.
+
+//Collection Framework we can only store non-primitive type of data.(its mean it only stores the object).
+//ArrayList a=new ArrayList<>() a.add(10)-->we can insert 10.it is a object (using wrapper class).
+//We can store the heterogeneous  type of data.(mean we can store different type data).
+//We can increase and decrease the size of collection framework.
+//Collection framework api which provide predefine classes ,interface and method.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+//* DataStructure -: It is the way by which we can store   the data in  efficient way. (efficient->time , space).
+//* Type of DS - > There are two type : 1.Primitive  2.Non-Primitive
+//Primitive -> 1.boolean 2.char 3.int 4.byte 5.short 6.int 7.long 8.float 9.double
+//Non-Primitive-> 1.Linear  2.Non-Linear
+//  Linear ->String , arrays , list , set  , queue , Arraylist , hashset , Linked Hash set etc.
+//Non-Linear -> Graph , tree
+
+
+//API -> API (Application Programming Interface) in Java is a set of predefined rules and specifications for
+// accessing a web-based software application or web tool.It offers a way for developers to interact or
+// integrate two different systems, allowing for easy exchange of data and communication.
+
+//*  API (Application Programming Interface) is a set of rules and specifications that allow two
+// applications to communicate.
